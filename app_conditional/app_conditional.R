fc6b356321f5f25fb577e16025ec3b989e28e428
rikkeuldbaek <rikke123uldbaek@hotmail.com>
2023-04-20 12:12:54 +0200

    yo

--- app_conditional/app_conditional.R
+++ app_conditional/app_conditional.R
@@ -15,20 +15,24 @@ pacman::p_load(shiny, # R shiny functions
 data <- read_excel("./data/cult_activities_aarhus.xls")
 data$Latitude <- as.numeric(data$Latitude)
 data$Longitude <- as.numeric(data$Longitude)
-ls.str(data)
 
 
 # Define UI
 ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Cultural Activities in Aarhus"),
   
   # Add leaflet map
   leafletOutput("map"),
   
   # Add filter widgets
   selectInput("age_group", "Age group", choices = c("all", "children", "adults")),
-  selectInput("category", "Activity category", choices = c("museum", "venue", "playground", "bylivshus"))
+  selectInput("category", "Activity category", choices = c("all","museum", "venue", "playground", "bylivshus")),
+  selectInput("inside_outside", "Inside or Outside", choices = c("both", "inside", "outside"))
   
 )
+######### fluid page END
 
 # Define server
 server <- function(input, output) {
@@ -47,8 +51,9 @@ server <- function(input, output) {
   filtered_data <- reactive({
     data %>%
       filter(
-        if (input$age_group != "All") data$group == input$age_group else TRUE,
-        if (input$category != "All") data$Type == input$category else TRUE
+        if (input$age_group != "all") data$group == input$age_group else TRUE,
+        if (input$category != "all") data$Type == input$category else TRUE,
+        if (input$inside_outside != "both") data$inside_outside== input$inside_outside else TRUE
       )
   })
   
@@ -57,8 +62,9 @@ server <- function(input, output) {
     leafletProxy("map", data = filtered_data()) %>%
       clearMarkers() %>%
       addMarkers(lat = ~Latitude, lng = ~Longitude,
-        popup = paste("Age group: ", data$group, "<br>",
-                      "Category: ", data$Type)
+                 label = ~Placename,
+                 #popup = paste("Age group: ", data$group, "<br>",
+                 #     "Category: ", data$Type)
       )
   })
   
@@ -66,3 +72,4 @@ server <- function(input, output) {
 
 # Run the app
 shinyApp(ui, server)
+
